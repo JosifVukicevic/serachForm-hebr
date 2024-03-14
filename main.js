6afab795cd7979f789a86689843b5cb782cda296
Josif Vukićević <josifjole32@gmail.com>
2024-03-14 14:33:11 +0100

    things to do and charter
    Ostalo Things to do i Charter da se odradi. Nakon toga, validacija glavnog dugmeta "Search", promjena jezika na hebrejski i povezivanje linkova.
    Na Cars umjesto samog imena lokacije dodati brojeve.

--- main.js
+++ main.js
@@ -51,74 +51,104 @@ function setActiveTabItem(tabId) {
 
 
 // VILLAS TAB
-/*
-const cities = ["Bay of Kotor", "Budva", "Tivat", "Kotor", "Herceg Novi", "Bar", "Petrovac", "Ulcinj", "Podgorica", "Montenegro Coastline", "Montenegro North", "Central Montenegro"];
-const types = ["Villa", "Apartment", "Hotel room"];
 
-const cityInput = document.getElementById('cityInput');
-const typeInput = document.getElementById('villas-type');
+// tab going to
 
-const cityList = document.getElementById('myDropdown1');
-const typeList = document.getElementById('myDropdown2');
+document.addEventListener('DOMContentLoaded', function () {
+    var inputField = document.getElementById('villas-type');
+    var dropdown = document.getElementById('myDropdown2');
+    var links = dropdown.getElementsByTagName('a');
 
-let selectedCity = "";
-let selectedType = "";
-
-cityInput.addEventListener('input', function () {
-    const inputValue = cityInput.value.toLowerCase();
-    const filteredCities = cities.filter(city => city.toLowerCase().includes(inputValue));
-    displayCities(filteredCities, cityList);
-});
-
-cityInput.addEventListener('focus', function () {
-    displayCities(cities, cityList);
-});
-
-typeInput.addEventListener('input', function () {
-    const inputValue = typeInput.value.toLowerCase();
-    const filteredTypes = types.filter(type => type.toLowerCase().includes(inputValue));
-    displayTypes(filteredTypes, typeList);
-});
-
-typeInput.addEventListener('focus', function () {
-    displayTypes(types, typeList);
-});
-
-function displayCities(citiesArray, list) {
-    list.innerHTML = "";
-    citiesArray.forEach(city => {
-        const listItem = document.createElement('a');
-        listItem.textContent = city;
-        listItem.addEventListener('click', function () {
-            selectedCity = city;
-            cityInput.value = city;
-            list.innerHTML = "";
-            updateUrlAndSubmit();
-            console.log("Selected city:", selectedCity);
-        });
-        list.appendChild(listItem);
+    inputField.addEventListener('click', function () {
+        dropdown.style.display = 'block';
     });
+
+    inputField.addEventListener('input', function () {
+        var inputText = inputField.value.toLowerCase();
+
+        for (var i = 0; i < links.length; i++) {
+            var link = links[i];
+            var cityName = link.textContent.toLowerCase();
+
+            if (cityName.indexOf(inputText) === -1) {
+                link.style.display = 'none';
+            } else {
+                link.style.display = 'block';
+            }
+        }
+    });
+
+    inputField.addEventListener('blur', function () {
+        for (var i = 0; i < links.length; i++) {
+            links[i].style.display = 'block';
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var cityInput = document.getElementById("cityInput");
+    var dropdownContent = document.getElementById("myDropdown1");
+
+    cityInput.addEventListener("input", function () {
+        var value = this.value.toLowerCase();
+        var dropdownItems = dropdownContent.getElementsByTagName("a");
+        for (var i = 0; i < dropdownItems.length; i++) {
+            var cityName = dropdownItems[i].innerText.toLowerCase();
+            if (cityName.indexOf(value) > -1) {
+                dropdownItems[i].style.display = "";
+            } else {
+                dropdownItems[i].style.display = "none";
+            }
+        }
+        if (value === "") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+
+    dropdownContent.addEventListener("click", function (e) {
+        if (e.target.tagName === "A") {
+            var selectedCity = e.target.innerText;
+            cityInput.value = selectedCity;
+            console.log(selectedCity);
+            dropdownContent.style.display = "none";
+        }
+    });
+
+    cityInput.addEventListener("click", function () {
+        this.value = "";
+    });
+});
+
+// Tip - tab 2
+
+function setType(type) {
+    document.getElementById('villas-type').value = type;
+    document.getElementById('myDropdown2').style.display = 'none';
 }
 
-function displayTypes(typesArray, list) {
-    list.innerHTML = "";
-    typesArray.forEach(type => {
-        const listItem = document.createElement('a');
-        listItem.textContent = type;
-        listItem.addEventListener('click', function () {
-            selectedType = type;
-            typeInput.value = type;
-            list.innerHTML = "";
-            updateUrlAndSubmit();
-            console.log("Selected type:", selectedType);
-        });
-        list.appendChild(listItem);
-    });
+function toggleDropdown() {
+    var dropdown = document.getElementById('myDropdown2');
+    if (dropdown.style.display === 'none' || dropdown.style.display === '') {
+        dropdown.style.display = 'block';
+    } else {
+        dropdown.style.display = 'none';
+    }
 }
-*/
 
+document.addEventListener('click', function (event) {
+    var dropdownContainer = document.getElementById('dropdown-container');
+    var targetElement = event.target;
 
-// Travelers validacija
+    var isClickInsideDropdown = dropdownContainer.contains(targetElement) || targetElement.id === 'myDropdown2';
+
+    if (!isClickInsideDropdown) {
+        document.getElementById('myDropdown2').style.display = 'none';
+    }
+});
+
+// Travelers validacija - tab 2
 
 var dropdownContent = document.getElementById('myDropdown3');
 var travelersCon = document.getElementById('valueForTravelers');
@@ -206,9 +236,333 @@ window.onload = function () {
 
 // kalendar
 
-var data = formatData();
-console.log(data)
+document.addEventListener('DOMContentLoaded', function () {
+    function handleDateChanges(tabId) {
+        var startDateInput = document.getElementById('startDate-' + tabId);
+        var endDateInput = document.getElementById('endDate-' + tabId);
 
+        startDateInput.addEventListener('change', function () {
+            if (startDateInput.value) {
+                var startDateValue = startDateInput.value;
+                console.log('Start date for tab ' + tabId + ':', startDateValue);
+            }
+        });
+
+        // Provjeravamo da li postoji endDateInput prije dodavanja event listenera
+        if (endDateInput) {
+            endDateInput.addEventListener('change', function () {
+                if (endDateInput.value) {
+                    var endDateValue = endDateInput.value;
+                    console.log('End date for tab ' + tabId + ':', endDateValue);
+                }
+            });
+        }
+    }
+
+    var tabIds = ['tab2', 'tab3', 'tab4', 'tab6'];
+    tabIds.forEach(function (tabId) {
+        handleDateChanges(tabId);
+    });
+});
+
+// TAB 3
+
+// pick up input
+
+document.addEventListener("DOMContentLoaded", function () {
+    var inputField = document.getElementById("pickUp");
+    var dropdown = document.getElementById("myDropdown-pickUp");
+    var locations = dropdown.getElementsByTagName("a");
+
+    inputField.addEventListener("click", function () {
+        dropdown.style.display = "block";
+    });
+
+    dropdown.addEventListener("click", function (event) {
+        if (event.target.tagName === "A") {
+            var selectedLocation = event.target.textContent;
+            inputField.value = selectedLocation;
+            dropdown.style.display = "none";
+        }
+    });
+
+    document.addEventListener("click", function (event) {
+        if (event.target !== inputField && event.target.parentNode !== dropdown) {
+            dropdown.style.display = "none";
+        }
+    });
+
+    inputField.addEventListener("input", function () {
+        var searchTerm = inputField.value.toLowerCase();
+        for (var i = 0; i < locations.length; i++) {
+            var location = locations[i].textContent.toLowerCase();
+            if (location.includes(searchTerm)) {
+                locations[i].style.display = "block";
+            } else {
+                locations[i].style.display = "none";
+            }
+        }
+        if (inputField.value === "") {
+            for (var i = 0; i < locations.length; i++) {
+                locations[i].style.display = "block";
+            }
+            dropdown.style.display = "block";
+        }
+    });
+});
+
+// same pick up input
+
+document.addEventListener("DOMContentLoaded", function () {
+    var inputField = document.getElementById("same-pickUp");
+    var dropdown = document.getElementById("myDropdown-samePickUp");
+    var locations = dropdown.getElementsByTagName("a");
+
+    inputField.addEventListener("click", function () {
+        dropdown.style.display = "block";
+    });
+
+    dropdown.addEventListener("click", function (event) {
+        if (event.target.tagName === "A") {
+            var selectedLocation = event.target.textContent;
+            inputField.value = selectedLocation;
+            dropdown.style.display = "none";
+        }
+    });
+
+    document.addEventListener("click", function (event) {
+        if (event.target !== inputField && event.target.parentNode !== dropdown) {
+            dropdown.style.display = "none";
+        }
+    });
+
+    inputField.addEventListener("input", function () {
+        var searchTerm = inputField.value.toLowerCase();
+        for (var i = 0; i < locations.length; i++) {
+            var location = locations[i].textContent.toLowerCase();
+            if (location.includes(searchTerm)) {
+                locations[i].style.display = "block";
+            } else {
+                locations[i].style.display = "none";
+            }
+        }
+        if (inputField.value === "") {
+            for (var i = 0; i < locations.length; i++) {
+                locations[i].style.display = "block";
+            }
+            dropdown.style.display = "block";
+        }
+    });
+});
+
+// time - pick up i drop off
+
+document.addEventListener("DOMContentLoaded", function () {
+    generateTimeOptions("dropdown-for-cars-pickUpTime");
+    generateTimeOptions("dropdown-for-cars-dropOffTime");
+    generateTimeOptions("dropdown-for-transfers-pickUpTime");
+
+    function generateTimeOptions(elementId) {
+        var dropdown = document.getElementById(elementId);
+
+        for (var hour = 0; hour <= 23; hour++) {
+            for (var minutes = 0; minutes <= 30; minutes += 30) {
+                var time = formatTime(hour) + ":" + formatTime(minutes);
+                var option = document.createElement("a");
+                option.setAttribute("href", "#");
+                option.textContent = time;
+                dropdown.appendChild(option);
+            }
+        }
+    }
+
+    function formatTime(time) {
+        return (time < 10 ? "0" : "") + time;
+    }
+});
+
+
+// funkcionalnosti za time - cars tab
+
+document.addEventListener("DOMContentLoaded", function () {
+    var pickTimeDiv = document.getElementById("main-div-for-pickTime");
+    var dropTimeDiv = document.getElementById("main-div-for-dropTime");
+
+    var pickTimeInput = pickTimeDiv.querySelector(".input-for-time");
+    var pickTimeDropdown = document.getElementById("dropdown-for-cars-pickUpTime");
+    var dropTimeInput = dropTimeDiv.querySelector(".input-for-time");
+    var dropTimeDropdown = document.getElementById("dropdown-for-cars-dropOffTime");
+
+    var selectedTime;
+
+    function openDropdown(dropdown) {
+        dropdown.style.display = "block";
+    }
+
+    function closeDropdown(dropdown) {
+        dropdown.style.display = "none";
+    }
+
+    function setTime(input, time) {
+        input.value = time;
+    }
+
+    pickTimeDiv.addEventListener("click", function () {
+        openDropdown(pickTimeDropdown);
+    });
+
+    dropTimeDiv.addEventListener("click", function () {
+        openDropdown(dropTimeDropdown);
+    });
+
+    pickTimeInput.addEventListener("click", function (event) {
+        event.stopPropagation();
+        openDropdown(pickTimeDropdown);
+    });
+
+    dropTimeInput.addEventListener("click", function (event) {
+        event.stopPropagation();
+        openDropdown(dropTimeDropdown);
+    });
+
+    document.addEventListener("click", function (event) {
+        if (event.target !== pickTimeInput && event.target !== dropTimeInput) {
+            closeDropdown(pickTimeDropdown);
+            closeDropdown(dropTimeDropdown);
+        }
+    });
+
+    pickTimeDropdown.addEventListener("click", function (event) {
+        if (event.target.tagName === "A") {
+            selectedTime = event.target.textContent;
+            setTime(pickTimeInput, selectedTime);
+            closeDropdown(pickTimeDropdown);
+        }
+    });
+
+    dropTimeDropdown.addEventListener("click", function (event) {
+        if (event.target.tagName === "A") {
+            selectedTime = event.target.textContent;
+            setTime(dropTimeInput, selectedTime);
+            closeDropdown(dropTimeDropdown);
+        }
+    });
+});
+
+// transfers tab
+
+// departure
+
+document.addEventListener("DOMContentLoaded", function () {
+    var inputField = document.getElementById("departureTransfers");
+    var dropdown = document.getElementById("departure-transfers");
+    var locations = dropdown.getElementsByTagName("a");
+
+    inputField.addEventListener("click", function () {
+        dropdown.style.display = "block";
+    });
+
+    dropdown.addEventListener("click", function (event) {
+        if (event.target.tagName === "A") {
+            var selectedLocation = event.target.textContent;
+            inputField.value = selectedLocation;
+            dropdown.style.display = "none";
+        }
+    });
+
+    document.addEventListener("click", function (event) {
+        if (event.target !== inputField && event.target.parentNode !== dropdown) {
+            dropdown.style.display = "none";
+        }
+    });
+
+    inputField.addEventListener("input", function () {
+        var searchTerm = inputField.value.toLowerCase();
+        for (var i = 0; i < locations.length; i++) {
+            var location = locations[i].textContent.toLowerCase();
+            if (location.includes(searchTerm)) {
+                locations[i].style.display = "block";
+            } else {
+                locations[i].style.display = "none";
+            }
+        }
+        if (inputField.value === "") {
+            for (var i = 0; i < locations.length; i++) {
+                locations[i].style.display = "block";
+            }
+            dropdown.style.display = "block";
+        }
+    });
+});
+
+// arrival
+
+document.addEventListener("DOMContentLoaded", function () {
+    var inputField = document.getElementById("arrivalTransefrs");
+    var dropdown = document.getElementById("arrival-transfers");
+    var locations = dropdown.getElementsByTagName("a");
+
+    inputField.addEventListener("click", function () {
+        dropdown.style.display = "block";
+    });
+
+    dropdown.addEventListener("click", function (event) {
+        if (event.target.tagName === "A") {
+            var selectedLocation = event.target.textContent;
+            inputField.value = selectedLocation;
+            dropdown.style.display = "none";
+        }
+    });
+
+    document.addEventListener("click", function (event) {
+        if (event.target !== inputField && event.target.parentNode !== dropdown) {
+            dropdown.style.display = "none";
+        }
+    });
+
+    inputField.addEventListener("input", function () {
+        var searchTerm = inputField.value.toLowerCase();
+        for (var i = 0; i < locations.length; i++) {
+            var location = locations[i].textContent.toLowerCase();
+            if (location.includes(searchTerm)) {
+                locations[i].style.display = "block";
+            } else {
+                locations[i].style.display = "none";
+            }
+        }
+        if (inputField.value === "") {
+            for (var i = 0; i < locations.length; i++) {
+                locations[i].style.display = "block";
+            }
+            dropdown.style.display = "block";
+        }
+    });
+});
+
+// pick up time
+
+document.addEventListener("DOMContentLoaded", function () {
+    var input = document.getElementById("pick-time-input-transfers");
+    var dropdown = document.getElementById("dropdown-for-transfers-pickUpTime");
+
+    input.addEventListener("click", function (event) {
+        dropdown.style.display = "block";
+        event.stopPropagation();
+    });
+
+    document.addEventListener("click", function (event) {
+        if (!dropdown.contains(event.target) && event.target !== input) {
+            dropdown.style.display = "none";
+        }
+    });
+
+    dropdown.addEventListener("click", function (event) {
+        if (event.target.tagName === "A") {
+            input.value = event.target.textContent;
+            dropdown.style.display = "none";
+        }
+    });
+});
 
 
 // Search dugme - funkcija za prosljedjivanje na odgovarajuci url
@@ -229,14 +583,25 @@ function updateUrlAndSubmit() {
                 var base_url = 'https://montenegrovillas.com/en';
                 window.location.href = base_url;
                 console.log("usao")
-                
-                var query_parameters = '?q='+ location + '&from=' + fromDate + '&to=' + toDate + '&type=' + numberOfType;
+
+                var query_parameters = '?q=' + location + '&from=' + fromDate + '&to=' + toDate + '&type=' + numberOfType;
                 url = base_url + query_parameters + data;
-                
+
                 break;
             case '#tab3':
                 var base_url = 'www.montenegrocars.com/en/';
                 var query_parameters = '?';
+
+                var locationNumber;
+
+                if (location === "Tivat Bus Station") {
+                    locationNumber = "205";
+                } else if (location === "Porto Montenegro") {
+                    locationNumber = "208";
+                } else {
+                    location = location
+                }
+
                 url = 'www.montenegrocars.com/en/?goingTo=Tivat&type=Car';
                 break;
             case '#tab4':
@@ -260,20 +625,3 @@ function updateUrlAndSubmit() {
     }
 }
 
-/*
-        var allFieldsFilled = true;
-        var inputFields = activeTab.querySelectorAll('input[type="text"], select');
-        inputFields.forEach(function(input) {
-            if (!input.value) {
-                allFieldsFilled = false;
-            }
-        });
-
-        if (allFieldsFilled) {
-            window.location.href = url;
-        } else {
-            console.log('Molimo Vas da popunite sva polja.');
-        }
-    }
-}
-*/
