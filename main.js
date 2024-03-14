00b79a4f973d5963dc40563496a2949b53ff9ea8
Josif Vukićević <josifjole32@gmail.com>
2024-03-14 08:20:09 +0100

    comm

--- main.js
+++ main.js
@@ -49,8 +49,9 @@ function setActiveTabItem(tabId) {
     }
 }
 
-// VILLAS TAB
 
+// VILLAS TAB
+/*
 const cities = ["Bay of Kotor", "Budva", "Tivat", "Kotor", "Herceg Novi", "Bar", "Petrovac", "Ulcinj", "Podgorica", "Montenegro Coastline", "Montenegro North", "Central Montenegro"];
 const types = ["Villa", "Apartment", "Hotel room"];
 
@@ -114,24 +115,20 @@ function displayTypes(typesArray, list) {
         list.appendChild(listItem);
     });
 }
-
-function updateUrlAndSubmit() {
-    var url = window.location.href.split('#')[0] + `#${selectedCity}-${selectedType}`;
-    window.location.href = url;
-}
+*/
 
 
 // Travelers validacija
 
-
 var dropdownContent = document.getElementById('myDropdown3');
 var travelersCon = document.getElementById('valueForTravelers');
 var fullDivTravelers = document.getElementById('main-travelers-div');
+var icondropDown = document.getElementById('icon-drop');
 
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
     var targetElement = event.target;
 
-    var isClickInsideDropdown = dropdownContent.contains(targetElement) || targetElement === travelersCon || targetElement === fullDivTravelers;
+    var isClickInsideDropdown = dropdownContent.contains(targetElement) || targetElement === travelersCon || targetElement === fullDivTravelers || targetElement === icondropDown;
 
     if (!isClickInsideDropdown) {
         dropdownContent.style.visibility = 'hidden';
@@ -141,32 +138,39 @@ document.addEventListener('click', function(event) {
 });
 
 
-
 function formatData() {
-    var formattedData = '';
     var adultsCount = document.getElementById('adults-count').textContent;
     var adultsSmallCount = document.getElementById('adults-small-count').textContent;
     var infantsCount = document.getElementById('infants-count').textContent;
     var bedroomsCount = document.getElementById('bedrooms-count').textContent;
     var bathroomsCount = document.getElementById('bathrooms-count').textContent;
     var petsAllowed = document.querySelector('.checkbox').checked;
+    var petsAllowedNumber;
 
-    formattedData = 'Adults: ' + adultsCount + ', ' +
-                    'Adults (2-12 yo): ' + adultsSmallCount + ', ' +
-                    'Infants: ' + infantsCount + ', ' +
-                    'Bedrooms: ' + bedroomsCount + ', ' +
-                    'Bathrooms: ' + bathroomsCount + ', ' +
-                    'Pets Allowed: ' + petsAllowed;
+    if (petsAllowed) {
+        petsAllowedNumber = 1;
+    } else {
+        petsAllowedNumber = 0;
+    }
+
+    var numberOfGuests = parseInt(adultsCount) + parseInt(adultsSmallCount) + parseInt(infantsCount);
+
+    var formattedData = '&guests=' + numberOfGuests + '&adults=' + adultsCount +
+        '&children=' + adultsSmallCount +
+        '&infants=' + infantsCount +
+        '&bedrooms=' + bedroomsCount +
+        '&bathrooms=' + bathroomsCount +
+        '&pets=' + petsAllowedNumber;
 
     return formattedData;
 }
 
-window.onload = function() {
+window.onload = function () {
     var plusButtons = document.querySelectorAll('.button-plus');
     var minusButtons = document.querySelectorAll('.button-minus');
 
-    plusButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
+    plusButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
             var travelerCountElement = this.parentElement.querySelector('span[id$="-count"]');
             var currentCount = parseInt(travelerCountElement.textContent);
             travelerCountElement.textContent = currentCount + 1;
@@ -175,8 +179,8 @@ window.onload = function() {
         });
     });
 
-    minusButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
+    minusButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
             var travelerCountElement = this.parentElement.querySelector('span[id$="-count"]');
             var currentCount = parseInt(travelerCountElement.textContent);
             if (currentCount > 0) {
@@ -188,14 +192,88 @@ window.onload = function() {
     });
 
     var checkbox = document.querySelector('.checkbox');
-    checkbox.addEventListener('change', function() {
+    checkbox.addEventListener('change', function () {
         console.log(formatData());
         document.getElementById('valueForTravelers').value = formatData();
     });
-
+    var dropdownContent = document.getElementById('myDropdown3');
     var applyButton = document.querySelector('.button-finTravelers button');
-    applyButton.addEventListener('click', function() {
-        console.log(formatData());
-        document.getElementById('valueForTravelers').value = formatData();
+    applyButton.addEventListener('click', function () {
+        console.log("pronadjeno")
+        dropdownContent.style.visibility = "hidden";
     });
 };
+
+// kalendar
+
+var data = formatData();
+console.log(data)
+
+
+
+// Search dugme - funkcija za prosljedjivanje na odgovarajuci url
+
+function updateUrlAndSubmit() {
+    var activeTab = document.querySelector('.nav-item.active');
+
+    if (activeTab) {
+        var tabId = activeTab.getAttribute('href');
+        var data = formatData();
+        var url;
+
+        switch (tabId) {
+            case '#tab1':
+                console.log("hotels")
+                break;
+            case '#tab2':
+                var base_url = 'https://montenegrovillas.com/en';
+                window.location.href = base_url;
+                console.log("usao")
+                
+                var query_parameters = '?q='+ location + '&from=' + fromDate + '&to=' + toDate + '&type=' + numberOfType;
+                url = base_url + query_parameters + data;
+                
+                break;
+            case '#tab3':
+                var base_url = 'www.montenegrocars.com/en/';
+                var query_parameters = '?';
+                url = 'www.montenegrocars.com/en/?goingTo=Tivat&type=Car';
+                break;
+            case '#tab4':
+                var base_url = 'www.montenegrotransfers.com/en/';
+                var query_parameters = '?';
+                url = 'www.montenegrotansfers.com/en/?goingTo=Tivat&type=Transfer';
+                break;
+            case '#tab5':
+                var base_url = 'www.montenegroactivities.com/en/';
+                var query_parameters = '?';
+                url = 'www.montenegroactivities.com/en/?goingTo=Tivat&type=Activity';
+                break;
+            case '#tab6':
+                var base_url = 'www.montenegrocharter.com/en/';
+                var query_parameters = '?';
+                url = 'www.montenegrocharter.com/en/?goingTo=Tivat&type=Boat';
+                break;
+            default:
+                return;
+        }
+    }
+}
+
+/*
+        var allFieldsFilled = true;
+        var inputFields = activeTab.querySelectorAll('input[type="text"], select');
+        inputFields.forEach(function(input) {
+            if (!input.value) {
+                allFieldsFilled = false;
+            }
+        });
+
+        if (allFieldsFilled) {
+            window.location.href = url;
+        } else {
+            console.log('Molimo Vas da popunite sva polja.');
+        }
+    }
+}
+*/
